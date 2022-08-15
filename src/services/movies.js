6841ed06672059fd2e808fcb5e93d4fc4e787163
KermitTheSapo <rjsf06@gmail.com>
2022-08-15 10:42:35 -0300

    title e sinopse funcionando

--- src/services/movies.js
+++ src/services/movies.js
@@ -8,13 +8,43 @@ import axios from "axios";
 const FilmesApi = axios.create({
     baseURL: "https://api.themoviedb.org/3/movie/popular?api_key=3ec3b78e7bcb7df233c8d24e81fd8592&language=pt-BR&page=1"
 })
-console.log(FilmesApi)
 
 export default class Movies extends Component {
+    state = {
+        movies: []
+    }
+
+    componentDidMount(){
+        this.getMovies()
+    }
+
+    getMovies = async () => {
+        const resposta = await FilmesApi.get()
+        console.log(resposta)
+        const allFilmes = resposta.data.results.map((item) => {
+            return{
+                // nome: item.original_title,
+                // sinopse: item.overview,
+                ...item,
+                nome: item.overview
+            }
+        })
+        this.setState({
+            movies: allFilmes
+        })
+    }
+
     render(){
         return(
             <div>
-                <h1>aaaaaa</h1>
+                {this.state.movies.map((item, index) => (
+                    <ul key={index}>
+                        {/* <li>{item.nome}</li> */}
+                        {/* <li>{item.sinopse}</li> */}
+                        <li>{item.original_title}</li>
+                        <li>{item.overview}</li>
+                    </ul>
+                ))}
             </div>
         )
     }
